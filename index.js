427bd566d9e13929a6f38a66a8e668253e47cf74
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-03-30 15:57:38 -0300

    feat(Req2) criado as rotas para products e sales

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const express = ('express');
+const app = express();
+const productsRouter = require('./routers/productsRouter');
+const salesRouter = require('./routers/salesRouter');
 require('dotenv').config();
+
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRouter);
+
+app.use('/sales', salesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
---
+++ routers/productsRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/');
+
+router.get('/');
+
+router.put('/');
+
+router.delete('/');
+
+module.exports = router;
