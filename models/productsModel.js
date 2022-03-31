328eaa83334f52bfdea571a206fce906073b8787
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-03-31 14:20:59 -0300

    feat(Req3) Retirado a variiavel de ambiante do database

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,14 +2,14 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const [products] = await connection.execute(`
-  SELECT * FROM products ORDER BY id;
+  SELECT * FROM StoreManager.products ORDER BY id;
   `);
   return products;
 };
 
 const getById = async (id) => {
   const [product] = await connection.execute(`
-  SELECT * FROM products WHERE id = ?;
+  SELECT * FROM StoreManager.products WHERE id = ?;
   `,
   [id]);
   return product[0];
--- models/salesModel.js
+++ models/salesModel.js
@@ -10,8 +10,8 @@ const serialize = (data) => (
 
 const getAll = async () => {
   const [sales] = await connection.execute(`
-  SELECT sp.sale_id, s.date, sp.product_id, sp.quantity FROM sales s
-  INNER JOIN sales_products sp
+  SELECT sp.sale_id, s.date, sp.product_id, sp.quantity FROM StoreManager.sales s
+  INNER JOIN StoreManager.sales_products sp
   ON s.id = sp.sale_id
   ORDER BY sp.sale_id, s.id;
   `);
@@ -20,8 +20,8 @@ const getAll = async () => {
 
 const getById = async (id) => {
   const [sale] = await connection.execute(`
-  SELECT s.date, sp.product_id, sp.quantity FROM sales s
-  INNER JOIN sales_products sp
+  SELECT s.date, sp.product_id, sp.quantity FROM StoreManager.sales s
+  INNER JOIN StoreManager.sales_products sp
   ON s.id = sp.sale_id
   WHERE s.id = ?
   ORDER BY sp.sale_id, sp.product_id;
