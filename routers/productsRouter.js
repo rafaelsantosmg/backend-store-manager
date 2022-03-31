b13f3617b1d0965d273a1f5d937aa9e1a4abd9b0
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-03-31 14:22:05 -0300

    feat(Req3) implementado as funções post e put nas rotas

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { getAll, getById } = require('../controllers/productsController');
+const { getAll,
+  getById,
+  create,
+  update } = require('../controllers/productsController');
+const validProducts = require('../middlewares/validateProductMiddleware');
 
 router.get('/', getAll);
 
 router.get('/:id', getById);
+
+router.post('/', validProducts, create);
+
+router.put('/:id', validProducts, update);
 
 module.exports = router;
--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { getAll, getById } = require('../controllers/salesControllers');
+const { getAll,
+  getById,
+  create,
+  update } = require('../controllers/salesControllers');
+const validateSales = require('../middlewares/validateSaleMiddleware');
 
 router.get('/', getAll);
 
 router.get('/:id', getById);
+
+router.post('/', validateSales, create);
+
+router.put('/:id', validateSales, update);
 
 module.exports = router;
