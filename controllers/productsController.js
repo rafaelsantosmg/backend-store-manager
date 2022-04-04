e778be36fb329af3831a1fc1cecc918dadcac709
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-04-04 16:09:12 -0300

    refactor(ProductController/Sercice) Refatorada as funções para tratar o erro com throw e try catch

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,41 +6,45 @@ const getAll = async (_req, res) => {
 };
 
 const getById = async (req, res) => {
-  const { id } = req.params;
-  const product = await ProductsService.getById(Number(id));
-  if (product === undefined || product.length === 0) {
-    return res.status(404).json({ message: 'Product not found' });
+  try {
+    const { id } = req.params;
+    const product = await ProductsService.getById(Number(id));
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-  return res.status(200).json(product);
 };
 
 const create = async (req, res) => {
-  const { name, quantity } = req.body;
-  const product = await ProductsService.create({ name, quantity });
-  if (product !== undefined && product.length === 0) {
-    return res.status(409).json({ message: 'Product already exists' });
+  try {
+    const { name, quantity } = req.body;
+    const product = await ProductsService.create({ name, quantity });
+    return res.status(201).json({ id: product[0], name, quantity });
+  } catch (error) {
+    return res.status(409).json({ message: error.message });
   }
-  return res.status(201).json({ id: product[0], name, quantity });
 };
 
 const update = async (req, res) => {
-  const { id } = req.params;
-  const { name, quantity } = req.body;
-  const product = await ProductsService
-    .update({ name, quantity, id: Number(id) });
-  if (product !== undefined && product.length === 0) {
-    return res.status(404).json({ message: 'Product not found' });
+  try {
+    const { id } = req.params;
+    const { name, quantity } = req.body;
+    const product = await ProductsService
+      .update({ name, quantity, id: Number(id) });
+    return res.status(200).json(product[0]);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-  return res.status(200).json(product[0]);
 };
 
 const destroyer = async (req, res) => {
-  const { id } = req.params;
-  const product = await ProductsService.destroyer({ id: Number(id) });
-  if (product !== undefined && product.length === 0) {
-    return res.status(404).json({ message: 'Product not found' });
+  try {
+    const { id } = req.params;
+    await ProductsService.destroyer({ id: Number(id) });
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-  return res.status(204).end();
 };
 
 module.exports = {
--- services/productsService.js
+++ services/productsService.js
@@ -7,28 +7,32 @@ const getAll = async () => {
 
 const getById = async (id) => {
   const [product] = await ProductsModel.getById(id);
+  if (!product) throw Error('Product not found');
   return product;
 };
 
 const create = async ({ name, quantity }) => {
   const [findProduct] = await ProductsModel.getFindName(name);
-  if (findProduct) return [];
+  if (findProduct) throw Error('Product already exists');
   const { insertId } = await ProductsModel.create({ name, quantity });
   return [insertId];
 };
 
 const update = async ({ name, quantity, id }) => {
   const findProductId = await ProductsModel.getFindId(id);
-  if (findProductId !== undefined && findProductId.length === 0) return [];
+  if (findProductId !== undefined && findProductId.length === 0) {
+    throw Error('Product not found');
+  }
   await ProductsModel.update({ name, quantity, id });
   return [{ id, name, quantity }];
 };
 
 const destroyer = async ({ id }) => {
   const findProductId = await ProductsModel.getFindId(id);
-  if (findProductId !== undefined && findProductId.length === 0) return [];
+  if (findProductId !== undefined && findProductId.length === 0) {
+    throw Error('Product not found');
+  }
   await ProductsModel.destroyer({ id });
-  return id;
 };
 
 module.exports = {
